4d5389f2478b93e79bd992ebafda993024f350ec
Jufri <optibizpro@hotmail.com>
2025-01-11 05:53:33 +0700

    nav

--- frappe/public/js/frappe/ui/toolbar/about.js
+++ frappe/public/js/frappe/ui/toolbar/about.js
@@ -5,7 +5,6 @@ frappe.ui.misc.about = function () {
 
 		$(d.body).html(
 			repl(
-<<<<<<< HEAD
 				"<div>\
 		<p>" +
 					__("Open Source Applications for the Web") +
@@ -26,37 +25,6 @@ frappe.ui.misc.about = function () {
 		<hr>\
 		<p class='text-muted'>&copy; Frappe Technologies Pvt. Ltd. and contributors </p> \
 		</div>",
-=======
-				`<div>
-					<p>${__("Open Source Applications for the Web")}</p>
-					<p><i class='fa fa-globe fa-fw'></i>
-						${__("Website")}:
-						<a href='https://frappeframework.com' target='_blank'>https://frappeframework.com</a></p>
-					<p><i class='fa fa-github fa-fw'></i>
-						${__("Source")}:
-						<a href='https://github.com/frappe' target='_blank'>https://github.com/frappe</a></p>
-					<p><i class='fa fa-graduation-cap fa-fw'></i>
-						Frappe School: <a href='https://frappe.school' target='_blank'>https://frappe.school</a></p>
-					<p><i class='fa fa-linkedin fa-fw'></i>
-						Linkedin: <a href='https://linkedin.com/company/frappe-tech' target='_blank'>https://linkedin.com/company/frappe-tech</a></p>
-					<p><i class='fa fa-twitter fa-fw'></i>
-						Twitter: <a href='https://twitter.com/frappetech' target='_blank'>https://twitter.com/frappetech</a></p>
-					<p><i class='fa fa-youtube fa-fw'></i>
-						YouTube: <a href='https://www.youtube.com/@frappetech' target='_blank'>https://www.youtube.com/@frappetech</a></p>
-					<hr>
-					<h4>${__("Installed Apps")}</h4>
-					<div id='about-app-versions'>${__("Loading versions...")}</div>
-					<p>
-						<b>
-							<a href="/attribution" target="_blank" class="text-muted">
-								${__("Dependencies & Licenses")}
-							</a>
-						</b>
-					</p>
-					<hr>
-					<p class='text-muted'>${__("&copy; Frappe Technologies Pvt. Ltd. and contributors")} </p>
-					</div>`,
->>>>>>> fc1c3f895a2bbd99dd7a0574de180a4095b6e41b
 				frappe.app
 			)
 		);
@@ -80,24 +48,14 @@ frappe.ui.misc.about = function () {
 			var $wrap = $("#about-app-versions").empty();
 			$.each(Object.keys(versions).sort(), function (i, key) {
 				var v = versions[key];
-<<<<<<< HEAD
 				if (v.branch) {
 					var text = $.format("<p><b>{0}:</b> v{1} ({2})<br></p>", [
-=======
-				let text;
-				if (v.branch) {
-					text = $.format("<p><b>{0}:</b> v{1} ({2})<br></p>", [
->>>>>>> fc1c3f895a2bbd99dd7a0574de180a4095b6e41b
 						v.title,
 						v.branch_version || v.version,
 						v.branch,
 					]);
 				} else {
-<<<<<<< HEAD
 					var text = $.format("<p><b>{0}:</b> v{1}<br></p>", [v.title, v.version]);
-=======
-					text = $.format("<p><b>{0}:</b> v{1}<br></p>", [v.title, v.version]);
->>>>>>> fc1c3f895a2bbd99dd7a0574de180a4095b6e41b
 				}
 				$(text).appendTo($wrap);
 			});
